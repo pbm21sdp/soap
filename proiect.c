d78709512a8f05230aa7e3d3b19ca48081e52c74
Negru Briana-Maria <briana.negru@student.upt.ro>
2024-04-03 09:59:30 +0300

    Week7

--- proiect.c
+++ proiect.c
@@ -2,7 +2,14 @@
 Saptamana 1: Utilizatorul poate specifica directorul pe care doreste sa il monitorizeze in linia de comanda, ca prim argument. 
 Se vor urmari atat modificarile aparute la nivelul directorului, cat si la nivelul subarborelui. Se va parcurge directorul si intregul 
 sau subrbore si se face un SNAPSHOT. Acest snapshot poate fi un fisier, care contine toate datele pe care le furnizeaza directorul
-si subarborele si care sunt considerate necesare.
+si subarborele si care sunt considerate necesare. Facem un director separat (daca vrem) de cel dat ca argument unde adaugam toate snapshot-urile obtinute.
+La fiecare director obtin un snapshot diferit.
+Saptamana 2: Se va rula programul de mai multe ori. La fiecare rulare comparam snapshot-urile curente cu snapshot-urile de data trecuta.
+Daca s-a facut o modificare, inlocuim snapshot-ul vechi cu cel nou.
+Se actualizeaza functionalitatea programului astfel incat sa primeasca un numar nespecificat de argumente in linie de comanda (oricate, 
+dar nu mai mult de 10), cu mentiunea ca niciun argument nu se va repeta. Programul proceseaza doar directoarele si ignora alte tipuri de 
+argumente. Programul va primi un argument suplimentar care va fi un director de iesire, care va stoca toate snapshot-urile argumentelor
+date la intrare. Directorul de iesire va avea in fata lui un -o, ca sa fie evidentiat.
 
     MAIN SUCCESS SCENARIO-ish
 
@@ -23,6 +30,30 @@ si subarborele si care sunt considerate necesare.
 #include <fcntl.h>
 #include <unistd.h>
 #include <libgen.h> 
+
+//structura care va retine informatiile despre un regular file
+typedef struct fisier{
+    char nume[256]; //numele fisierului
+    off_t dimensiune; //dimensiune totala in bytes
+    time_t data_modificare; //momentul ultimei modificari (st_mtim din struct stat)
+    ino_t inode; //numarul inode-ului
+}fisier;
+
+//structura care va retine informatiile despre un director
+typedef struct director{
+    char nume[256]; //numele directorului
+    off_t dimensiune; //dimensiune totala in bytes
+    time_t data_modificare; //momentul ultimei modificari (st_mtim din struct stat)
+    ino_t inode; //numarul inode-ului
+}director;
+
+//structura care va retine informatiile despre o legatura simbolica
+typedef struct legatura_simbolica{
+    char nume[256]; //numele directorului
+    off_t dimensiune; //dimensiune totala in bytes
+    time_t data_modificare; //momentul ultimei modificari (st_mtim din struct stat)
+    ino_t inode; //numarul inode-ului
+}legatura_simbolica;
 
 void eroare(const char *mesaj) //functie care primeste ca parametru un String si semnaleaza o eroare de executie sau de logica 
 {
@@ -47,7 +78,7 @@ void parcurgere_recursiva(const char *director, int snapi) //primeste ca paramet
         eroare("Directorul pe care il voiam curent nu a putut fi deschis.");
     }
 
-    /*2. se citeste pe rand cate o intrare din director, apeland functia readdir, Fiecare apel al acestei functii va returna un pointer la
+    /*2. se citeste pe rand cate o intrare din director, apeland functia readdir. Fiecare apel al acestei functii va returna un pointer la
     o structura struct dirent in care se vor gasi informatii despre intrarea in director citita. Intrarile vor fi parcurse una dupa alta 
     pana cand se ajunge la ultima inregistrare. In momentul in care nu mai exista inregistrari in directorul citit, functia readdir va 
     returna NULL. Singura informatie care poate fi extrasa (conform POSIX) din structura dirent este numele intrarii in director. Toate 
@@ -55,18 +86,81 @@ void parcurgere_recursiva(const char *director, int snapi) //primeste ca paramet
 
     //inainte de orice citire am nevoie de zona de memorie care va stoca toate informatiile, adica struct dirent 
 
-    struct dirent *dst; //dst de la director struct nu de la destination
+    struct dirent *dst; //dst de la director struct nu de la destination  
 
     //trebuie sa apelez readdir pana cand ajung la ultima inregistrare din director, dupa care primesc ca return value NULL cand am epuizat inregistrarile
     //readdir returneaza un pointer la o structura struct dirent, deci dst al meu va fi de fapt readdir
 
-    while(1)
+    while(1) //while(readdir != NULL)
     {
         dst = readdir(subdir); //la fiecare iteratie completez structura cu date despre obiectul analizat la acel moment
 
         if(dst == NULL) //daca avem NULL ca return value inseamna ca nu mai sunt inregistrari in director 
         {
+            printf("S-a ajuns la finalul directorului.\n"); //afisam un mesaj ca indicator pentru sfarsitul unui subdirector/director
+            closedir(subdir); //inchidem subdirectorul daca am terminat de citit 
             break; //s-a incheiat parcurgerea directorului, deci putem iesi din loop-ul infinit
+        }
+
+        struct stat st; 
+
+        if(lstat(subdir, &st) == -1) //daca lstat returneaza -1 inseamna ca a aparut o eroare
+        {
+            eroare("Eroare la determinarea informatiilor despre entitatea curenta, ceva s-a intamplat cu lstat."); //apelam functia eroare si precizam ce mai exact nu a mers
+        }
+
+        if(S_ISDIR(st.st_mode) == 0) //verifica daca entitatea curenta este un director
+        {
+            struct director d; //definim o structura care retine informatii despre un director
+            strcpy(d.nume, dst->d_name);
+            d.dimensiune = st.st_size;
+            d.data_modificare = st.st_mtime;
+            d.inode = st.st_ino;
+
+            char buffer[sizeof(d)]; //initializez un buffer care sa retina toate informatiile pe care vreau sa le scriu in snapshot
+            sprintf(buffer, "%s %ld %ld %ld\n", d.nume, d.dimensiune, d.data_modificare, d.inode); //folosesc sprintf ca sa obtin un string formatat asa cum vreau eu
+            
+            ssize_t written_bytes = write(snapi, buffer, sizeof(buffer));
+            if (written_bytes == -1) 
+            {
+                eroare("Eroare la scrierea informatiilor despre director in snapshot.");
+            }
+            
+            parcurgere_recursiva(dst->d_name, snapi); //daca este director, se apeleaza recursiv functia pentru a parcurge directorul gasit 
+        }//concateneaza pt cale
+        else if(S_ISREG(st.st_mode) == 0) //verifica daca entitatea curenta este un fisier
+        {
+            struct fisier fis; //definim o structura care retine informatii despre un fisier regular
+            strcpy(fis.nume, dst->d_name);
+            fis.dimensiune = st.st_size;
+            fis.data_modificare = st.st_mtime;
+            fis.inode = st.st_ino;
+
+            char buffer[sizeof(fis)]; //initializez un buffer care sa retina toate informatiile pe care vreau sa le scriu in snapshot
+            sprintf(buffer, "%s %ld %ld %ld\n", fis.nume, fis.dimensiune, fis.data_modificare, fis.inode); //folosesc sprintf ca sa obtin un string formatat asa cum vreau eu
+            
+            ssize_t written_bytes = write(snapi, buffer, sizeof(buffer));
+            if (written_bytes == -1) 
+            {
+                eroare("Eroare la scrierea informatiilor despre fisier in snapshot.");
+            }
+        }
+        else if(S_ISLNK(st.st_mode) == 0) //verifica daca entitatea curenta este o legatura simbolica 
+        {
+            struct legatura_simbolica leg; //definim o structura care retine informatii despre o legatura simbolica 
+            strcpy(leg.nume, dst->d_name);
+            leg.dimensiune = st.st_size;
+            leg.data_modificare = st.st_mtime;
+            leg.inode = st.st_ino;
+
+            char buffer[sizeof(leg)]; //initializez un buffer care sa retina toate informatiile pe care vreau sa le scriu in snapshot
+            sprintf(buffer, "%s %ld %ld %ld\n", leg.nume, leg.dimensiune, leg.data_modificare, leg.inode); //folosesc sprintf ca sa obtin un string formatat asa cum vreau eu
+            
+            ssize_t written_bytes = write(snapi, buffer, sizeof(buffer));
+            if (written_bytes == -1) 
+            {
+                eroare("Eroare la scrierea informatiilor despre legatura simbolica in fisierul snapshot.");
+            }
         }
     }
 }
@@ -101,7 +195,7 @@ int main(int argc, char *argv[]) //arguments count(argc) specifica nr argumentel
 
     //daca nu s-a oprit din executie programul inseamna ca apelul lstat a reusit, deci am obtinut informatii despre director
     //prin intermediul campului st_mode din st, putem verifica daca intr-adevar am primit un director ca parametru sau daca avem altceva
-    //folosim macro-ul S_ISDIR(m) ca sa verificam daca tipul fisierului primit este director
+    //folosim macro-ul S_ISDIR(m) ca sa verificam daca tipul fisierului primit este director 
     //in cazul nostru m este st.st_mode
     
     if(S_ISDIR(st.st_mode) != 0) //daca valoarea returnata nu este 0 inseamna ca nu am primit ca argument un director
@@ -140,7 +234,7 @@ int main(int argc, char *argv[]) //arguments count(argc) specifica nr argumentel
     //in caz de succes, creat returneaza un descriptor de fisier
     //in caz de eroare, returneaza -1
 
-    int snapi = creat(nume_snapshot, mod);
+    int snapi = creat(nume_snapshot, mod); //pune open pt tania
     if(snapi == -1)
     {
         eroare("Eroare la crearea si/sau deschiderea fisierului de snapshot."); //ori nu s-a creat, ori nu s-a deschis, cert e ca n-a mers
@@ -159,7 +253,7 @@ int main(int argc, char *argv[]) //arguments count(argc) specifica nr argumentel
     //inchid si fisierul de snapshot pentru ca am adaugat tot ce era nevoie in el
     //functia close returneaza 0 in caz de succes, -1 in caz de eroare
 
-    if(close(nume_snapshot) == -1)
+    if(close(snapi) == -1)
     {
         eroare("Eroare la inchiderea snapshot-ului."); //alta self-explanatory
     }
